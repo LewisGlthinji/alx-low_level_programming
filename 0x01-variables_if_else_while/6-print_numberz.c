03fd2bbb756c5d8db4a9f731cef1b591f04c0722
LewisGlthinji <lewissgithinji@gmail.com>
2023-03-20 19:04:08 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers 
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+int n = 48;
+while (n <= 57)
+{
+putchar(n);
+n += 1;
+}
+printf("\n");
+return (0);
+}
