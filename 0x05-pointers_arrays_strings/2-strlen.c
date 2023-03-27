c794b004ece91ca4ab62f826e834985459c99e69
LewisGlthinji <lewissgithinji@gmail.com>
2023-03-27 13:39:19 -0700

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,14 +6,16 @@
  * Return: it returns the length
  */
 
-int _strlen (char *s)
+int _strlen(char *s)
 {
 	int longi = 0;
 
-	while (*s! = '\0')
+	while (*s != '\0')
 	{
 		longi++;
 		s++;
 	}
-	return (longi)
+
+	return (longi);
 }
+
