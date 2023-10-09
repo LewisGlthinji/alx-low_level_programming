64bbd391e74746b2b68bae1b11d2bc593a1cd502
LewisGlthinji <lewissgithinji@gmail.com>
2023-10-09 12:36:35 -0700

    this is my 0x00 hello world.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * puts - used to print a string
+ * main - this is the container
+ */
+
+int main
+{
+	put("Programming is like building a multilingual puzzle");
+	return (0);
+}
+
